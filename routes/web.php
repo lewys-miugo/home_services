b0dff46f2894824b73a98a709c4cfc7e15dad559
lewys-miugo <lewiswambugu01@gmail.com>
2023-11-07 08:45:41 +0300

    Grouping routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Livewire\Admin\AdminDashboardComponent;
+use App\Livewire\Customer\CustomerDashboardComponent;
 use App\Livewire\HomeComponent;
+use App\Livewire\Sprovider\SproviderDashboardComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,12 +23,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',HomeComponent::class)->name('home');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+// For customer
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {
+    Route::get('/my/dashboard',CustomerDashboardComponent::class)->name('user.dashboard');
+});
+
+// For Service Provider
+Route::middleware(['auth:sanctum', 'authsprovider',config('jetstream.auth_session'),'verified',])->group(function () {
+    Route::get('/serviceprovider/dashboard',SproviderDashboardComponent::class)->name('svp.dashboard');
+});
+
+// For admin
+Route::middleware(['auth:sanctum','authadmin',config('jetstream.auth_session'),'verified',])->group(function () {
+    Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
 });
